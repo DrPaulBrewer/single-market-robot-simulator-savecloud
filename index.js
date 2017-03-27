a6a03712155441b2a5c76a32070953daee2f490e
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2017-03-27 04:39:59 +0000

    rewrite to use known npm stream functions; also refactor to use split off pipe-to-storage

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 
 const storage = require('@google-cloud/storage')();  // without an API key, this only works in google cloud
 
-const Readable = require('stream').Readable;
+const Readable = require('readable-stream').Readable;
+const intoStream = require('into-stream');
+const pipeToStorage = require('pipe-to-storage')(storage);
 
 /* custom Readable Stream closely follows example at https://nodejs.org/api/stream.html#stream_an_example_counting_stream */
 
@@ -30,52 +32,9 @@ class LogStream extends Readable {
 	    else if ((typeof str==='string') && (str.length>0))
 		hungry = this.push(str, 'utf8');
 	    else
-		throw new Error("expected string, got: "+typeof(str)+" i is "+i+" len is "+this._max);
+		throw new Error("LogStream._read() expected string, got: "+typeof(str)+" i is "+i+" len is "+this._max);
 	} while ((i<this._max) && hungry);
     }
-}
-
-class StringStream extends Readable {
-    constructor(s, opt){
-	super(opt);
-	this._s = s;
-    }
-    
-    _read() {
-	if (this._s){
-	    const hungry = this.push(this._s, 'utf8');
-	    delete this._s;
-	    if (hungry) this.push(null);
-	} else {
-	    this.push(null);
-	}
-    }
-}
-
-class JSONStream extends StringStream {
-    constructor(_obj){
-	super(JSON.stringify(_obj, null, 2));
-    }
-}
-
-function promiseToSaveStream(localStream, bucketName, fileName){
-    return new Promise(function(resolve, reject){
-	const remote = (storage
-			.bucket(bucketName)
-			.file(fileName)
-			.createWriteStream({resumable:false})
-		       );
-	localStream.on('end', function(){
-	    resolve();
-	});
-	localStream.on('error', function(e){
-	    // remote.end();
-	    console.log("error while writing "+bucketName+"://"+fileName);
-	    console.log("error caught in promiseToSaveStream:"+e);
-	    reject(e);
-	});
-	localStream.pipe(remote);
-    });
 }
 
 module.exports = function savecloud(sim){
@@ -83,15 +42,15 @@ module.exports = function savecloud(sim){
     const bucket = sim.config.gcloud.bucket;
     const dir = (sim.config.gcloud.dir || '')+'/';
     function promiseToSaveLog(logname){
-	return promiseToSaveStream(new LogStream(sim.logs[logname]),
-				   bucket,
-				   dir+logname+'.csv');
+	return pipeToStorage(new LogStream(sim.logs[logname]),
+			     bucket,
+			     dir+logname+'.csv');
     }
     function promiseToSaveSimConfig(){
 	if (sim.config.gcloud) delete sim.config.gcloud;
-	return promiseToSaveStream(new JSONStream(sim.config),
-				   bucket,
-				   dir+'sim.json');
+	return pipeToStorage(intoStream(JSON.stringify(sim.config)),
+			     bucket,
+			     dir+'sim.json');
     }
     if (typeof(sim)!=='object')
 	throw new Error("missing simulation parameter to savecloud");
